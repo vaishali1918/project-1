271e55247275d0291d38b3bf366a9a831b0074fc
Vaishali Agrawal <vaishali.agrawal_cs22@gla.ac.in>
2025-08-13 16:22:46 +0530

    Delete HRMS/backend/routes directory

--- HRMS/backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const { register, login, logout} = require("../controller/authController")
-
-const router = express.Router()
-router.post("/login", login)
-router.post("/logout", logout)
-
-module.exports = router
